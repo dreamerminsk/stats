53416da1bcc6e8ff895fc1ec2d6e62002d6c5cfd
dreamerminsk <tutmirage@gmail.com>
2019-03-31 10:31:48 +0300

    Rename and move TorrentParser

--- gui-thread.py
+++ gui-thread.py
@@ -1,116 +1,18 @@
 ﻿import sys
 from datetime import datetime
-from typing import Dict, Any
-from urllib.parse import urlparse, parse_qs
 
 import requests
 from PySide2.QtCore import QTimer, Signal, Slot, Qt, QModelIndex, QAbstractTableModel, QThread, QAbstractItemModel
 from PySide2.QtGui import QColor
 from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QTabWidget, QListWidget, QSplitter, QTableView, \
     QTreeView
+from PySide2.QtWidgets import QStyleFactory
 from PySide2.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout
 from bs4 import BeautifulSoup
 
 import rutracker
-import utils
 from source import DataSource
 from workers import RssWorker
-
-
-class TorrentParser:
-    @staticmethod
-    def get_title(doc):
-        title = None
-        t = doc.select_one('h1.maintitle')
-        if t:
-            title = t.text.strip()
-        return title
-
-    @staticmethod
-    def get_forum(doc):
-        forum = None
-        for item in doc.select('table.w100 a'):
-            q = urlparse(item.get('href')).query
-            ps = parse_qs(q)
-            if 'f' in ps:
-                forum = ps['f'][0]
-        return forum
-
-    @staticmethod
-    def get_seed(doc):
-        seed = 0
-        seed_item = doc.select_one('.seed b')
-        if seed_item:
-            seed = int(seed_item.text.strip())
-        return seed
-
-    @staticmethod
-    def get_leech(doc):
-        leech = 0
-        leech_item = doc.select_one('.leech b')
-        if leech_item:
-            leech = int(leech_item.text.strip())
-        return leech
-
-    @staticmethod
-    def get_published(doc):
-        published = None
-        pi = doc.select_one('p.post-time a')
-        if pi:
-            published = utils.parse_date(pi.text.strip())
-        return published
-
-    @staticmethod
-    def get_last_modified(doc):
-        lm = None
-        lmi = doc.select_one('.posted_since')
-        if lmi:
-            idx = lmi.text.find('ред. ')
-            if idx > 0:
-                val = lmi.text[idx + 5:idx + 20]
-                lm = utils.parse_date(val)
-        return lm
-
-    @staticmethod
-    def get_hash(doc):
-        tor_hash = None
-        hi = doc.select_one('#tor-tor_hash')
-        if hi:
-            tor_hash = hi.text.strip()
-        return tor_hash
-
-    @staticmethod
-    def get_downloads(doc):
-        downloads = 0
-        for n in doc.select('li'):
-            if n.text.startswith('Скачан'):
-                downloads = utils.extract_int(n.text)
-        return downloads
-
-    @staticmethod
-    def get_user(doc):
-        user = 0
-        for n in doc.select('a'):
-            if n.text.startswith('[Профиль]'):
-                q = urlparse(n.get('href')).query
-                ps = parse_qs(q)
-                if 'u' in ps:
-                    user = ps['u'][0]
-        return user
-
-    def parse(self, doc):
-        topic = dict()
-        topic['forum'] = self.get_forum(doc)
-        topic['title'] = self.get_title(doc)
-        topic['seed'] = self.get_seed(doc)
-        topic['leech'] = self.get_leech(doc)
-        topic['published'] = self.get_published(doc)
-        topic['last_modified'] = self.get_last_modified(doc)
-        topic['hash'] = self.get_hash(doc)
-        topic['downloads'] = self.get_downloads(doc)
-        topic['last_checked'] = datetime.now()
-        topic['user_id'] = self.get_user(doc)
-        return topic
 
 
 class NewTorrentLM(QAbstractTableModel):
@@ -183,13 +85,10 @@ class TorrentsWidget(QWidget):
         print(t)
         if 'id' not in t:
             return
-        doc, error = rutracker.get_topic(t['id'])
+        topic, error = rutracker.get_topic2(t['id'])
         if error is not None:
             print(error)
             return
-        parser = TorrentParser()
-        topic = parser.parse(doc)
-        topic['id'] = t['id']
         self.ds.insert_torrent(topic)
         self.torrents += 1
         self.newtorrents.emit(self.torrents)
@@ -228,13 +127,10 @@ class Torrents2Widget(QWidget):
         print(t)
         if 'id' not in t:
             return
-        doc, error = rutracker.get_topic(t['id'])
+        topic, error = rutracker.get_topic2(t['id'])
         if error is not None:
             print(error)
             return
-        parser = TorrentParser()
-        topic = parser.parse(doc)
-        topic['id'] = t['id']
         self.ds.insert_torrent(topic)
         self.torrents += 1
         self.newtorrents.emit(self.torrents)
@@ -446,97 +342,6 @@ class RssCategoryModel2(QAbstractItemModel):
         self.last_changed_items = [None, None, None]
 
 
-class RssCategoryModel(QAbstractItemModel):
-    cats: Dict[Any, Any]
-
-    def canFetchMore(self, parent):
-        return False
-
-    def columnCount(self, parent=None, *args, **kwargs):
-        return 3
-
-    def data(self, index, role=None):
-        if role == Qt.DisplayRole:
-            catname = ''
-            for i, key in enumerate(self.cats.keys()):
-                if i == index.row():
-                    catname = key
-            if index.column() == 0:
-                return self.cats[catname].category
-            elif index.column() == 1:
-                return self.cats[catname].forums
-            elif index.column() == 2:
-                return self.cats[catname].topics
-            else:
-                return None
-        return None
-
-    def fetchMore(self, parent):
-        pass
-
-    def hasChildren(self, parent=None, *args, **kwargs):
-        pass
-
-    def headerData(self, section, orientation, role=None):
-        if role != Qt.DisplayRole:
-            return None
-        if orientation == Qt.Horizontal:
-            return self.headers[section]
-        else:
-            return None
-
-    def index(self, row, column, parent=None, *args, **kwargs):
-        if not parent.isValid():
-            catname = ''
-            for i, key in enumerate(self.cats.keys()):
-                if i == row:
-                    catname = key
-            return self.createIndex(row, column, self.cats[catname])
-        return QModelIndex()
-
-    def insertRow(self, row, parent=None, *args, **kwargs):
-        pass
-
-    def insertRows(self, row, count, parent=None, *args, **kwargs):
-        pass
-
-    def itemData(self, index):
-        return {}
-
-    def parent(self, child):
-        return QModelIndex()
-
-    def rowCount(self, parent=None, *args, **kwargs):
-        return len(self.cats)
-
-    def setData(self, index, value, role=None):
-        return False
-
-    def setItemData(self, index, roles):
-        return False
-
-    def sort(self, column, order=None):
-        pass
-
-    def addCategory(self, category, torrents):
-        if category in self.cats:
-            self.beginResetModel()
-            self.cats[category].forums += 1
-            self.cats[category].topics += torrents
-            self.endResetModel()
-        else:
-            self.beginInsertRows(QModelIndex(), len(self.cats), len(self.cats))
-            item = RssCategoryItem(category, 1, torrents)
-            self.cats[category] = item
-            self.endInsertRows()
-
-    def __init__(self):
-        QAbstractItemModel.__init__(self)
-        self.headers = ['Категории', 'Форумы', 'Темы']
-        self.categories = []
-        self.cats = dict()
-
-
 class RssWidget(QWidget):
     newtorrents = Signal(int)
 
@@ -564,7 +369,6 @@ class RssWidget(QWidget):
     @Slot(int, int)
     def processed(self, forum_id, torrents):
         forum = self.ds.get_forum(forum_id)
-        print(forum)
         cat = self.ds.get_category(forum['category'])
         self.cat_model.addCategory(cat['title'], torrents)
 
@@ -623,6 +427,7 @@ class MyWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
     window = MyWindow()
     window.show()
     sys.exit(app.exec_())
--- rutracker.py
+++ rutracker.py
@@ -1,7 +1,11 @@
 import pickle
+from datetime import datetime
+from urllib.parse import urlparse, parse_qs
 
 import requests
 from bs4 import BeautifulSoup
+
+import utils
 
 LOGIN_URL = 'https://rutracker.org/forum/login.php'
 TOPIC_URL = 'https://rutracker.org/forum/viewtopic.php?t='
@@ -72,6 +76,113 @@ def get_topic(id):
     return get_page(url)
 
 
+def get_topic2(topic_id):
+    url = TOPIC_URL + str(topic_id)
+    doc, error = get_page(url)
+    if error is not None:
+        return (None, error)
+    parser = TopicParser()
+    topic = parser.parse(doc)
+    topic['id'] = topic_id
+    return (topic, None)
+
+
 def get_user(id):
     url = USER_URL + str(id)
     return get_page(url)
+
+
+class TopicParser:
+    @staticmethod
+    def get_title(doc):
+        title = None
+        t = doc.select_one('h1.maintitle')
+        if t:
+            title = t.text.strip()
+        return title
+
+    @staticmethod
+    def get_forum(doc):
+        forum = None
+        for item in doc.select('table.w100 a'):
+            q = urlparse(item.get('href')).query
+            ps = parse_qs(q)
+            if 'f' in ps:
+                forum = ps['f'][0]
+        return forum
+
+    @staticmethod
+    def get_seed(doc):
+        seed = 0
+        seed_item = doc.select_one('.seed b')
+        if seed_item:
+            seed = int(seed_item.text.strip())
+        return seed
+
+    @staticmethod
+    def get_leech(doc):
+        leech = 0
+        leech_item = doc.select_one('.leech b')
+        if leech_item:
+            leech = int(leech_item.text.strip())
+        return leech
+
+    @staticmethod
+    def get_published(doc):
+        published = None
+        pi = doc.select_one('p.post-time a')
+        if pi:
+            published = utils.parse_date(pi.text.strip())
+        return published
+
+    @staticmethod
+    def get_last_modified(doc):
+        lm = None
+        lmi = doc.select_one('.posted_since')
+        if lmi:
+            idx = lmi.text.find('ред. ')
+            if idx > 0:
+                val = lmi.text[idx + 5:idx + 20]
+                lm = utils.parse_date(val)
+        return lm
+
+    @staticmethod
+    def get_hash(doc):
+        tor_hash = None
+        hi = doc.select_one('#tor-tor_hash')
+        if hi:
+            tor_hash = hi.text.strip()
+        return tor_hash
+
+    @staticmethod
+    def get_downloads(doc):
+        downloads = 0
+        for n in doc.select('li'):
+            if n.text.startswith('Скачан'):
+                downloads = utils.extract_int(n.text)
+        return downloads
+
+    @staticmethod
+    def get_user(doc):
+        user = 0
+        for n in doc.select('a'):
+            if n.text.startswith('[Профиль]'):
+                q = urlparse(n.get('href')).query
+                ps = parse_qs(q)
+                if 'u' in ps:
+                    user = ps['u'][0]
+        return user
+
+    def parse(self, doc):
+        topic = dict()
+        topic['forum'] = self.get_forum(doc)
+        topic['title'] = self.get_title(doc)
+        topic['seed'] = self.get_seed(doc)
+        topic['leech'] = self.get_leech(doc)
+        topic['published'] = self.get_published(doc)
+        topic['last_modified'] = self.get_last_modified(doc)
+        topic['hash'] = self.get_hash(doc)
+        topic['downloads'] = self.get_downloads(doc)
+        topic['last_checked'] = datetime.now()
+        topic['user_id'] = self.get_user(doc)
+        return topic

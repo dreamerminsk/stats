d571ba846ca10aa9dd6afc0793e3683994706412
dreamerminsk <tutmirage@gmail.com>
2019-04-04 21:02:30 +0300

    Add viewer

---
+++ viewer.py
@@ -0,0 +1,29 @@
+﻿import sys
+
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QStyleFactory
+
+from source import DataSource
+from viewer.widgets import LeftMenuWidget
+
+
+class MyWindow(QMainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setWindowTitle("RuTracker.org")
+        self.setGeometry(200, 200, 640, 480)
+        self.tree = LeftMenuWidget()
+        self.setCentralWidget(self.tree)
+        self.ds = DataSource()
+
+    def closeEvent(self, event):
+        event.accept()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
--- viewer/widgets.py
+++ viewer/widgets.py
@@ -1,7 +1,49 @@
+from PySide2.QtCore import QObject, QAbstractItemModel, Qt, QModelIndex
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QSplitter, QTreeView
 
 from source import DataSource
 
+
+class CategoryItem(QObject):
+    pass
+
+
+class ForumItem(QObject):
+    pass
+
+
+class ForumModel(QAbstractItemModel):
+
+    def columnCount(self, parent=None, *args, **kwargs):
+        return 1
+
+    def data(self, index, role=None):
+        return None
+
+    def headerData(self, section, orientation, role=None):
+        if role != Qt.DisplayRole:
+            return None
+        if orientation == Qt.Horizontal:
+            return 'Title'
+        else:
+            return None
+
+    def index(self, row, column, parent=None, *args, **kwargs):
+        return QModelIndex()
+
+    def itemData(self, index):  # real signature unknown; restored from __doc__
+        """ itemData(self, index: PySide2.QtCore.QModelIndex) -> typing.Dict """
+        pass
+
+    def parent(self):
+        return QModelIndex()
+
+    def rowCount(self, parent=None, *args, **kwargs):  # real signature unknown; NOTE: unreliably restored from __doc__
+        return 1
+
+    def __init__(self, *args, **kwargs):
+        QAbstractItemModel.__init__(self)
+        self.ds = DataSource()
 
 class LeftMenuWidget(QWidget):
 
@@ -11,9 +53,7 @@ class LeftMenuWidget(QWidget):
         self.splitter = QSplitter(self)
         self.tree = QTreeView(self)
         self.tree.setSortingEnabled(True)
-        # self.treemodel = NewTorrentModel()
-        # self.list.setModel(self.listmodel)
+        self.tree.setModel(ForumModel())
         self.splitter.addWidget(self.tree)
         layout.addWidget(self.splitter)
         self.setLayout(layout)
-        self.ds = DataSource()

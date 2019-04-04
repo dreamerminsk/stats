d002eb71d7754338f7a139611d258ac488a0bc19
dreamerminsk <tutmirage@gmail.com>
2019-04-04 12:31:16 +0300

    new widgets

---
+++ viewer/widgets.py
@@ -0,0 +1,16 @@
+from PySide2.QtWidgets import QWidgets
+
+class LeftMenuWidget(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        layout = QVBoxLayout(self)
+        self.splitter = QSplitter(self)
+        self.tree = QTreeView(self)
+        self.tree.setSortingEnabled(True)
+        #self.treemodel = NewTorrentModel()
+        #self.list.setModel(self.listmodel)
+        self.splitter.addWidget(self.tree)
+        layout.addWidget(self.splitter)
+        self.setLayout(layout)
+        self.ds = DataSource()

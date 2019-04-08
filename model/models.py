096a28ca9f7fd70576777cc8e07a2b2050179065
dreamerminsk <tutmirage@gmail.com>
2019-04-08 08:03:28 +0300

    Small bug fixes

--- model/models.py
+++ model/models.py
@@ -9,7 +9,6 @@ class RssRootCategoryItem:
 
     def __init__(self) -> object:
         self.categories = []
-        self.is_last_changed = False
 
     def addChild(self, child):
         self.categories.append(child)
@@ -50,7 +49,6 @@ class RssCategoryItem:
         self.category = category
         self.forums = forums
         self.topics = topics
-        self.is_last_changed = False
 
     def child(self, index):
         return None
@@ -87,27 +85,7 @@ class RssCategoryModel(QAbstractItemModel):
         item = index.internalPointer()
         if role == Qt.DisplayRole:
             return item.data(index.column())
-        elif role == Qt.ForegroundRole:
-            if item.is_last_changed:
-                return QColor(Qt.blue)
-            else:
-                return QColor(Qt.black)
-        elif role == Qt.FontRole:
-            font = QApplication.font()
-            if item.is_last_changed:
-                font.setBold(True)
-                return font
-            else:
-                font.setBold(False)
-                return font
-
         return None
-
-    def hasChildren(self, parent=None):
-        if self.rowCount(parent) > 0:
-            return True
-        else:
-            return False
 
     def headerData(self, section, orientation, role=None):
         if role != Qt.DisplayRole:
@@ -156,23 +134,9 @@ class RssCategoryModel(QAbstractItemModel):
             return 1
         else:
             parentItem = parent.internalPointer()
-        return parentItem.childCount();
-
-    def setData(self, index, value, role=None):
-        return False
-
-    def setItemData(self, index, roles):
-        return False
+        return parentItem.childCount()
 
     def addCategory(self, category, torrents):
-        if self.last_changed_items[0]:
-            self.last_changed_items[0].is_last_changed = False
-            parentTopLeft = self.index(0, 0, QModelIndex())
-            childTopLeft = self.index(self.last_changed_items[0].row(), 0, parentTopLeft)
-            childBottomRight = self.index(self.last_changed_items[0].row(), 0, parentTopLeft)
-            self.dataChanged.emit(childTopLeft, childBottomRight)
-        self.last_changed_items[0] = self.last_changed_items[1]
-        self.last_changed_items[1] = self.last_changed_items[2]
         cat = None
         for item in self.rssroot.categories:
             if item.category == category:
@@ -180,28 +144,22 @@ class RssCategoryModel(QAbstractItemModel):
         if cat:
             cat.forums += 1
             cat.topics += torrents
-            cat.is_last_changed = True
-            self.last_changed_items[2] = cat
             parentTopLeft = self.index(0, 0, QModelIndex())
             parentBottomRight = self.index(0, 2, QModelIndex())
             childTopLeft = self.index(cat.row(), 0, parentTopLeft)
-            childBottomRight = self.index(cat.row(), 0, parentBottomRight)
+            childBottomRight = self.index(cat.row(), 2, parentBottomRight)
             self.dataChanged.emit(parentTopLeft, parentBottomRight)
             self.dataChanged.emit(childTopLeft, childBottomRight)
         else:
-            self.beginInsertRows(QModelIndex(), self.rssroot.childCount(), self.rssroot.childCount())
-            item = RssCategoryItem(self.rssroot, category, 1, torrents)
-            item.is_last_changed = True
-            self.last_changed_items[2] = item
-            self.rssroot.addChild(item)
+            self.beginInsertRows(self.index(0, 0, QModelIndex()), self.rssroot.childCount(), self.rssroot.childCount())
+            it = RssCategoryItem(self.rssroot, category, 1, torrents)
+            self.rssroot.addChild(it)
             self.endInsertRows()
 
     def __init__(self):
         QAbstractItemModel.__init__(self)
         self.headers = ['Category', 'Forums', 'Topics']
         self.rssroot = RssRootCategoryItem()
-        self.last_changed = None
-        self.last_changed_items = [None, None, None]
 
 
 class PublishedNewTorrent(QObject):

37686a370229bc2aba62feb851a98d9685208510
dreamerminsk <tutmirage@gmail.com>
2020-03-14 00:21:27 +0300

    tut.by

--- tutby/api.py
+++ tutby/api.py
@@ -1,3 +1,4 @@
+from time import sleep
 from urllib.parse import urlparse
 
 import requests
@@ -14,9 +15,23 @@ def get_page(ref):
         return None, exc
 
 
-doc, ex = get_page('http://tut.by/')
-for href in doc.select("a[href*='tut.by']"):
-    parsed_uri = urlparse(href.get('href'))
-    print(parsed_uri.netloc)
-for href in doc.select("img[src*='.jpg']"):
-    print(href)
+urls = {'http://tut.by/'}
+hosts = set()
+while len(urls) > 0:
+    url = urls.pop()
+    print("\t\t\t{} - {}".format(len(urls), url))
+    sleep(4)
+    doc, ex = get_page(url)
+    if ex is not None:
+        continue
+    print("\t\t\t{}".format(doc.select_one('title').text))
+    sleep(4)
+    for href in doc.select("a[href*='news.tut.by']"):
+        parsed_uri = urlparse(href.get('href'))
+        urls.add(href.get('href'))
+        hosts.add(parsed_uri.netloc)
+    for host in hosts:
+        print(host)
+    print("------------------------")
+    for href in doc.select("img[alt*='Фото']"):
+        print("{} - {}".format(href.get("alt"), href.get("src")))

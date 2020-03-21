49c2f617259c29a5481f635c09bd5f35d146bfcd
dreamerminsk <tutmirage@gmail.com>
2020-03-21 13:37:29 +0300

    TopicList

--- nnmclub/widgets.py
+++ nnmclub/widgets.py
@@ -10,20 +10,14 @@ class TopicView(QWidget):
         layout = QGridLayout(self)
 
         self.title = QLabel(topic.name)
-        self.title.setStyleSheet(
-            "QLabel{"
-            "border-width: 1px;"
-            "border-style: solid;"
-            "border-color: darkgrey;"
-            "border-radius: 4px;}")
         self.title.setStyleSheet("QLabel{font-size: 14px}")
         layout.addWidget(self.title, 0, 0, 1, 4, Qt.AlignLeading)
 
         self.like_box = QHBoxLayout(self)
         self.like_icon = QLabel()
         self.like_icon.setPixmap(QPixmap("images\\thanks_on.png"))
-        self.likes = QLabel("{} likes".format(topic.likes))
-        self.likes.setStyleSheet("QLabel{font-size: 14px;}")
+        self.likes = QLabel("{}".format(topic.likes))
+        self.likes.setStyleSheet("QLabel{font-size: 17px;}")
         self.like_box.addWidget(self.like_icon)
         self.like_box.addWidget(self.likes)
         layout.addLayout(self.like_box, 1, 0, 1, 4, Qt.AlignLeading)

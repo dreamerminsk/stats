a30a92f8541a2470961a713004680f189ee4fed9
dreamerminsk <tutmirage@gmail.com>
2020-03-24 12:27:35 +0300

    connection string

--- store/session.py
+++ store/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 # an Engine, which the Session will use for connection
 # resources
-some_engine = create_engine('postgresql://scott:tiger@localhost/')
+some_engine = create_engine('sqlite+pysqlite:///../nnmclub.db')
 
 # create a configured "Session" class
 Session = sessionmaker(bind=some_engine)
